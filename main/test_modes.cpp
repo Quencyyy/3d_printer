92927629cb0aba8d397b6dbc7b4cee9bf66cf7e4
HeiCha <105184746+Quencyyy@users.noreply.github.com>
2025-06-10 01:07:28 +0800

    Add Serial debug output for axis cycle test (#17)

--- main/test_modes.cpp
+++ main/test_modes.cpp
@@ -2,6 +2,7 @@
 #include "pins.h"
 #include "gcode.h"
 #include "button.h"
+#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
 // References to globals from main program
@@ -46,6 +47,7 @@ static const char axisChars[] = {'X', 'Y', 'Z', 'E'};
 
 void axisTestSetup() {
     showMessage("Axis Test", "Press Button");
+    Serial.println(F("Axis cycle test ready"));
 }
 
 void axisTestLoop() {
@@ -57,11 +59,13 @@ void axisTestLoop() {
             char buf[17];
             snprintf(buf, sizeof(buf), "Next: %c axis", axisChars[currentAxis]);
             showMessage(buf, "Press to Start");
+            Serial.println(buf);
         } else {
             moving = true;
             char buf[17];
             snprintf(buf, sizeof(buf), "Moving %c axis", axisChars[currentAxis]);
             showMessage(buf, "Press to Stop");
+            Serial.println(buf);
         }
     }
 
