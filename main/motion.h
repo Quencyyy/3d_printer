b75f6ee37d3d2cbcce296bde40b139605f424c8c
HeiCha <105184746+Quencyyy@users.noreply.github.com>
2025-07-22 16:40:56 +0800

    Simplify endstop handling (#59)

--- main/motion.h
+++ main/motion.h
@@ -3,4 +3,4 @@
 
 void moveAxis(int stepPin, int dirPin, long& pos, int target, int feedrate, char axis);
 
-void homeAxis(int stepPin, int dirPin, volatile bool &triggered, const char* label);
+void homeAxis(int stepPin, int dirPin, int endstopPin, const char* label);
