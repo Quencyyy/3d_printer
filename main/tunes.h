b0313a8ef7db692e9fc34331865b58a5fe8db9dc
HeiCha <105184746+Quencyyy@users.noreply.github.com>
2025-07-24 16:36:54 +0800

    Add compile-time tune selection (#82)

--- main/tunes.h
+++ main/tunes.h
@@ -1,6 +1,29 @@
 #pragma once
 #include <Arduino.h>
 
+// ----- Tune selection macros -----
+// Define exactly one of the following to select the tune included in firmware.
+// If none is defined, USE_TUNE_MARIO is the default.
+// Defining more than one will trigger a compile-time error.
+
+#if defined(USE_TUNE_MARIO) + defined(USE_TUNE_CANON) + defined(USE_TUNE_STAR_WARS) + defined(USE_TUNE_TETRIS) > 1
+#error "Only one USE_TUNE_* macro may be defined"
+#endif
+
+#if !(defined(USE_TUNE_MARIO) || defined(USE_TUNE_CANON) || defined(USE_TUNE_STAR_WARS) || defined(USE_TUNE_TETRIS))
+#define USE_TUNE_MARIO
+#endif
+
+#if defined(USE_TUNE_MARIO)
+#define DEFAULT_TUNE TUNE_MARIO
+#elif defined(USE_TUNE_CANON)
+#define DEFAULT_TUNE TUNE_CANON
+#elif defined(USE_TUNE_STAR_WARS)
+#define DEFAULT_TUNE TUNE_STAR_WARS
+#elif defined(USE_TUNE_TETRIS)
+#define DEFAULT_TUNE TUNE_TETRIS
+#endif
+
 
 // Enumeration of available completion tunes
 enum TuneType {
